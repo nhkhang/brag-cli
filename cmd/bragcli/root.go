91860c8cf8967ee394a422efb641af17820884a9
nhkhang <khangnguyenhoangcse@gmail.com>
2023-03-16 01:12:35 +0700

    Add view category

--- cmd/bragcli/root.go
+++ cmd/bragcli/root.go
@@ -46,8 +46,16 @@ var addCmd = &cobra.Command{
 var viewCmd = &cobra.Command{
 	Use: common.ViewCommand,
 	Run: func(cmd *cobra.Command, args []string) {
-		// message, _ := cmd.Flags().GetString("m")
-		category, err := category.GetCategory("default")
+		cat, err := cmd.Flags().GetString(string(common.CategoryFlag))
+		if err != nil {
+			return
+		}
+
+		if cat == "" {
+			cat = common.DefaultCategory
+		}
+
+		category, err := category.GetCategory(cat)
 		if err != nil {
 			fmt.Printf("Get category failed: %v", err)
 			return
